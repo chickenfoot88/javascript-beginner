cd5f73f555c6f0c13b54d54225165202a02198d3
aismagilov <aismagilov@c-i-p.ru>
2020-08-24 10:26:57 +0300

    add recipes fetch and display methods

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,45 +1,45 @@
 const baseEndpoint = 'http://www.recipepuppy.com/api';
-const proxy = `https://cors-anywhere.herokuapp.com/`;
+const proxy = 'https://cors-anywhere.herokuapp.com/';
 const form = document.querySelector('form.search');
 const recipesGrid = document.querySelector('.recipes');
 
-async function fetchRecipes(query) {
-  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
-  const data = await res.json();
-  return data;
-}
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  const el = event.currentTarget;
-  console.log(form.query.value);
-  fetchAndDisplay(form.query.value);
-}
-
-async function fetchAndDisplay(query) {
-  // turn the form off
-  form.submit.disabled = true;
-  // submit the search
-  const recipes = await fetchRecipes(query);
-  console.log(recipes);
-  form.submit.disabled = false;
-  displayRecipes(recipes.results);
-}
-
-function displayRecipes(recipes) {
-  console.log('Creating HTML');
+function displayRecepies(recipes) {
   const html = recipes.map(
-    recipe => `<div class="recipe">
-      <h2>${recipe.title}</h2>
-      <p>${recipe.ingredients}</p>
-      ${recipe.thumbnail &&
-        `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
-      <a href="${recipe.href}">View Recipe →</a>
-    </div>`
+    recipe =>
+      `
+        <div class="recipe">
+          <h2>${recipe.title}</h2>
+          <p>${recipe.ingredients}</p>
+          ${recipe.thumbnail &&
+            `<img
+              src="${recipe.thumbnail}"
+              alt="${recipe.title}"
+            />`}
+          <a href="${recipe.href}">View Recipe</a>
+        </div>
+      `
   );
   recipesGrid.innerHTML = html.join('');
 }
 
+async function fetchRecipes(query) {
+  const res = await fetch(`${proxy}${baseEndpoint}/?q=${query}`);
+  const data = await res.json();
+  return data;
+}
+
+async function fetchAndDisplay(searchTerm) {
+  const recipes = await fetchRecipes(searchTerm);
+  displayRecepies(recipes.results);
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  form.submit.disabled = true;
+  await fetchAndDisplay(form.query.value);
+  form.submit.disabled = false;
+}
+
 form.addEventListener('submit', handleSubmit);
-// on page load run it with pizza
+
 fetchAndDisplay('pizza');
